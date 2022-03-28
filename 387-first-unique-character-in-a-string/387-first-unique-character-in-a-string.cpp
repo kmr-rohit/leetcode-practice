858c76a97e61ef7745ff1e19d8b972f7819d90c1
rk972006 <80917122+rk972006@users.noreply.github.com>
2022-03-28 10:42:12 +0530

    Time: 68 ms (28.71%), Space: 10.9 MB (15.27%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int firstUniqChar(string s) {
-        map<char,int> m;
-        for(int i=0;i<s.size();i++){
-            m[s[i]]++;
+	int firstUniqChar(string s) {
+        unordered_map<char, pair<int, int>> m;
+        int idx = s.size();
+        for (int i = 0; i < s.size(); i++) {
+            m[s[i]].first++;
+            m[s[i]].second = i;
         }
-        for(int i = 0;i<s.size();i++){
-            if(m[s[i]]==1){
-                return i;
+        for (const auto& [c, p] : m) {
+            if (p.first == 1) {
+                idx = min(idx, p.second);
             }
         }
-        return -1;
-        
+        return idx == s.size() ? -1 : idx;
     }
-       
 };
