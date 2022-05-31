d19ce58380021c56509c6db2bf9a05d5092f1765
rk972006 <80917122+rk972006@users.noreply.github.com>
2022-05-31 18:08:57 +0530

    Time: 86 ms (86.30%), Space: 6 MB (93.35%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int solve(int n , vector<int> &dp){
-        if(n<=3){
-            return n;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        int ans=n;
+    //Memoised Approach
+//     int solve(int n , vector<int> &dp){
+//         if(n<=3){
+//             return n;
+//         }
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+//         int ans=n;
         
-        for(int i = 1 ; i*i <=n; i++){
-            ans = min(ans , 1+solve(n- i*i , dp));
-        }
-        dp[n] = ans;
-        return dp[n];
-    }
+//         for(int i = 1 ; i*i <=n; i++){
+//             ans = min(ans , 1+solve(n- i*i , dp));
+//         }
+//         dp[n] = ans;
+//         return dp[n];
+//     }
     int numSquares(int n) {
-        vector<int> dp(n+1 , -1);
-        return solve(n,dp);
+        int dp[n+1];
+        dp[0] = 0;
+        
+        for(int i=1;i<=n;++i)
+        {
+            dp[i] = i;
+            for(int j=1;j*j<=i;++j)
+            {
+                int sq = j*j;
+                dp[i] = min(dp[i],1+dp[i-sq]);
+            }
+        }
+        return dp[n];
+        
     }
 };
