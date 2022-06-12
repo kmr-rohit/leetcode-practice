838e5026b38dcd90a305faa55bde09ecfb01741b
rk972006 <80917122+rk972006@users.noreply.github.com>
2022-06-12 16:05:50 +0530

    Time: 262 ms (43.09%), Space: 76.2 MB (79.85%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,58 @@
+// Brute force Approach   : Is to iterate through the array , at any idx we have to run two for loops one to left and one to right to find smallest no in left and right tc - o(n^2)
+
+// Optimal : Using Monotonik Stack - creating leftSmall and rightSmall array and a stack to keep track of linearly increasing rectangles and determining the boundaries TC: O(2n) , SC: 0(3n);
+
+    // Code : 
+
+
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> s;
+        int leftSmall[n] , rightSmall[n];
+        
+        for(int i = 0;i<n;i++){
+            while(!s.empty() && heights[s.top()] >= heights[i]){
+                s.pop(); // poping indx at which rectangle is greater as it does not decide boundaries(heights);
+                
+            }
+            if(s.empty()){
+                leftSmall[i] = 0;
+            }
+            else{
+                leftSmall[i] = s.top()+1;
+               
+            }
+             s.push(i);
+        }
+        
+        // Clear the stack for reusing for rightSmall Array
+        while(!s.empty()) s.pop();
+        
+        // For rightSmall 
+        for(int i = n-1;i>=0;i--){
+            while(!s.empty() && heights[s.top()] >= heights[i]){
+                s.pop(); // poping indx at which rectangle is greater as it does not decide boundaries(heights);
+                
+            }
+            if(s.empty()){
+                rightSmall[i] = n-1;
+            }
+            else{
+                rightSmall[i] = s.top()-1;
+               
+            }
+             s.push(i);
+        }
+        int maxA = 0;//Track for area maximisation 
+        for(int i = 0;i<n;i++){
+            maxA  = max(maxA  ,(rightSmall[i] - leftSmall[i]+1)*heights[i]);
+            
+        }
+        
+        return maxA;
+        
+    }
+};
