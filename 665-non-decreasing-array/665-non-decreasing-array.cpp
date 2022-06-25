3d95b1d2b6086730a1de1c626af95773b292f4ca
rk972006 <80917122+rk972006@users.noreply.github.com>
2022-06-25 22:58:48 +0530

    Time: 56 ms (16.25%), Space: 27 MB (53.59%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int violation = 0;
+        for(int i=1;i<nums.size();i++){
+            // case for violation
+            if(nums[i-1] > nums[i]){
+                
+                // at max, we can have one violation. So if it has been occured before => return false
+                if(violation == 1){
+                    return false;
+                }
+                //if no prev violation, then violation++
+                violation++;
+               
+                // fixing violation => operation 1
+                if(i<2 || nums[i-2] <= nums[i])
+                    nums[i-1] = nums[i];
+                // fixing violation => operation 2
+                else
+                    nums[i] = nums[i-1];
+                
+            }
+        }
+        // if traversal of vector was successfull and return statement was not invoked then return true.
+        return true;
+    }
+};
