3c85d4d0c7677479e70c1320b4da88f8bce3382c
rk972006 <80917122+rk972006@users.noreply.github.com>
2022-07-05 18:25:52 +0530

    Time: 10 ms (96.66%), Space: 21.5 MB (40.86%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -9,27 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     // We will get the balanced tree when root is the middle element
+//     TreeNode* sortedArrayToBST(vector<int>& nums) {
+//         if(nums.size() == 0){
+//             return NULL;
+//         }
+//         else if(nums.size() == 1){
+//             TreeNode* node = new TreeNode(nums[0]);
+//             return node;
+//         }
+        
+//         int middle = nums.size()/2;
+        
+//         vector<int> leftints(nums.begin() , nums.begin() + middle);
+//         vector<int> rightints(nums.begin() + middle +1 , nums.end());
+        
+//         TreeNode * root = new TreeNode(nums[middle]);
+//         root->left = sortedArrayToBST(leftints);
+//         root->right = sortedArrayToBST(rightints);
+        
+//         return root;
+//     }
+// };
 class Solution {
 public:
-    // We will get the balanced tree when root is the middle element
+    using iter = vector<int>::const_iterator;
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        if(nums.size() == 0){
-            return NULL;
-        }
-        else if(nums.size() == 1){
-            TreeNode* node = new TreeNode(nums[0]);
-            return node;
-        }
+        if (nums.empty()) return nullptr;
         
-        int middle = nums.size()/2;
+        return buildBST(nums.begin(), nums.end());
+    }
+    
+    TreeNode* buildBST(iter b, iter e) {
+        if (b >= e) return nullptr;
         
-        vector<int> leftints(nums.begin() , nums.begin() + middle);
-        vector<int> rightints(nums.begin() + middle +1 , nums.end());
+        iter m = b + (e - b) / 2;
         
-        TreeNode * root = new TreeNode(nums[middle]);
-        root->left = sortedArrayToBST(leftints);
-        root->right = sortedArrayToBST(rightints);
+        TreeNode* node = new TreeNode(*m);
+        node->left = buildBST(b, m);
+        node->right = buildBST(m + 1, e);
         
-        return root;
+        return node;
     }
 };
