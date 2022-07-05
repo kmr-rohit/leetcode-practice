8ad70fa731a2d24932e4538e24929b54e6ca1882
rk972006 <80917122+rk972006@users.noreply.github.com>
2022-07-05 12:17:18 +0530

    Added solution - LeetHub

--- Count BST nodes that lie in a given range - GFG/count-bst-nodes-that-lie-in-a-given-range.cpp
+++ Count BST nodes that lie in a given range - GFG/count-bst-nodes-that-lie-in-a-given-range.cpp
@@ -85,29 +85,58 @@ Node* buildTree(string str)
 
  // } Driver Code Ends
 //Function to count number of nodes in BST that lie in the given range.
+//Brute force : Traversing all the nodes
+// class Solution{
+// public:
+//     int count = 0;
+//     void solve(Node*root , int l , int r){
+//         if(root == NULL){
+//           return;
+//       } 
+      
+//       solve(root->left , l , r );
+//       if(root->data >= l && root->data<= r){
+//           count++;
+         
+//       }
+//       solve(root->right , l , r);
+//     }
+//     int getCount(Node *root, int l, int h)
+//     {
+      
+//       solve(root , l , h);
+//       return count;
+//     }
+// };
+
+
+
+// Expected Approach : Just visiting node which are in the range 
 class Solution{
 public:
-    int count = 0;
-    void solve(Node*root , int l , int r){
-        if(root == NULL){
-          return;
-      } 
-      
-      solve(root->left , l , r );
-      if(root->data >= l && root->data<= r){
-          count++;
-         
-      }
-      solve(root->right , l , r);
-    }
-    int getCount(Node *root, int l, int h)
+    int getCount(Node* root, int low, int high)
     {
-      
-      solve(root , l , h);
-      return count;
+    	if (!root) 
+    	    return 0;
+    
+    	//if data at current node is equal to lower and upper range, we return 1.
+    	if (root->data == high && root->data == low)
+    		return 1;
+    
+    	//if data at current node is within range then we include it in count 
+    	//and call function recursively for its left and right children.
+    	if (root->data <= high && root->data >= low)
+    		return 1+getCount(root->left,low,high)+getCount(root->right,low,high);
+    
+    	//else if data at current node is smaller than lower range then
+    	//we call function recursively only for right child.
+    	else if (root->data < low)
+    		return getCount(root->right, low, high);
+    
+    	//else we call function recursively only for left child.
+    	else return getCount(root->left, low, high);
     }
 };
-
 
 // { Driver Code Starts.
 
