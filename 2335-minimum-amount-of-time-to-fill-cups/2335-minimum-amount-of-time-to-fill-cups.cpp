07368c56ca5138dd9301fed5adb451afd4683fb5
rk972006 <80917122+rk972006@users.noreply.github.com>
2022-07-20 11:20:09 +0530

    Time: 3 ms (83.85%), Space: 11.8 MB (53.28%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -1,28 +1,44 @@
+// class Solution {
+// public:
+//     int fillCups(vector<int>& a) {
+//         int n = a.size();
+        
+//         priority_queue<int> pq;
+        
+//         for(auto &x: a){
+//             if(x != 0) pq.push(x);
+//         }
+//         int ans = 0;
+//         while(pq.size() >= 2){
+//             int x = pq.top();
+//             pq.pop();
+//             int y = pq.top();
+//             pq.pop();
+            
+//             ans++;
+            
+//             if(x-1 != 0) pq.push(x-1);
+//             if(y-1 != 0) pq.push(y-1); 
+//         }
+        
+//         if(!pq.empty()) ans += pq.top();
+        
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int fillCups(vector<int>& a) {
-        int n = a.size();
-        
-        priority_queue<int> pq;
-        
-        for(auto &x: a){
-            if(x != 0) pq.push(x);
-        }
-        int ans = 0;
-        while(pq.size() >= 2){
-            int x = pq.top();
-            pq.pop();
-            int y = pq.top();
-            pq.pop();
+    int fillCups(vector<int>& amount) {
+        int ans=0;
+        while(1) {
+            sort(amount.begin(), amount.end());
+            if (amount.back()<=0)
+                break;
             
+            amount[1]--, amount[2]--;
             ans++;
-            
-            if(x-1 != 0) pq.push(x-1);
-            if(y-1 != 0) pq.push(y-1); 
         }
-        
-        if(!pq.empty()) ans += pq.top();
-        
         return ans;
     }
 };
