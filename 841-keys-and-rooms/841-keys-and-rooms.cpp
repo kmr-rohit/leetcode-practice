956bd8ab0e9452f3cc454b5cac296eb19e55bb40
rk972006 <80917122+rk972006@users.noreply.github.com>
2022-08-05 18:50:54 +0530

    Time: 19 ms (33.95%), Space: 10.6 MB (34.05%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,27 +1,47 @@
+// class Solution {
+// public:
+//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+//         vector<int> keys(rooms.size() , 0);
+//         int cnt = 1;
+//         keys[0] =1;
+        
+//         queue<int> q;
+//         q.push(0);
+//         while(!q.empty()){
+//             int node = q.front();
+//             q.pop();
+            
+//             for(int i =0;i<rooms[node].size();i++){
+//                 if(keys[rooms[node][i]]!=1){
+//                     keys[rooms[node][i]] = 1;
+//                     q.push(rooms[node][i]);
+//                     cnt++;
+//                 }
+//             }
+//         }
+        
+//         return (cnt == rooms.size());
+        
+        
+//     }
+// };
+
+
 class Solution {
 public:
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        vector<int> keys(rooms.size() , 0);
-        int cnt = 1;
-        keys[0] =1;
-        
-        queue<int> q;
-        q.push(0);
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            
-            for(int i =0;i<rooms[node].size();i++){
-                if(keys[rooms[node][i]]!=1){
-                    keys[rooms[node][i]] = 1;
-                    q.push(rooms[node][i]);
-                    cnt++;
-                }
-            }
+    void dfs(int curr, vector<int>& vis, vector<vector<int>>& rooms) {
+        vis[curr] = 1;
+        for(auto i : rooms[curr]) {
+            if(!vis[i]) dfs(i, vis, rooms);
         }
-        
-        return (cnt == rooms.size());
-        
-        
+    }
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<int> vis(n);
+        dfs(0, vis, rooms);
+        for(int i=0;i<n;i++) {
+            if(!vis[i]) return false;
+        }
+        return true;
     }
 };
